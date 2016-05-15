3c5e4457459f263acd0361b8ba5137a6617b546b
Dulio Denis <dulio.denis@yahoo.com>
2016-05-15 17:20:21 -0400

    Create a lastMessage computed attribute in the Chat NSManagedObject

--- V/V/Model/Chat.swift
+++ V/V/Model/Chat.swift
@@ -13,5 +13,32 @@ import CoreData
 class Chat: NSManagedObject {
 
 // Insert code here to add functionality to your managed object subclass
-
+    
+    // Last Message Optional Computed Attribute
+    
+    var lastMessage: Message? {
+        // setup a fetch request for Message instances
+        let request = NSFetchRequest(entityName: "Message")
+        // constrain the request with a predicate which specifies 
+        // that we only want Messages for the current chat instance
+        request.predicate = NSPredicate(format: "chat = %@", self)
+        // set a sort descriptor to use the timestamp of the message in descending order
+        request.sortDescriptors = [NSSortDescriptor(key: "timestamp", ascending: false)]
+        // only need 1 (the last one)
+        request.fetchLimit = 1
+        
+        // execute the request using a do {} catch
+        do {
+            // execute the request on the managed object context returning an array of Messages
+            guard let results = try self.managedObjectContext?.executeFetchRequest(request) as? [Message]
+                // returning nil if we get nothing which is fine since we specified an optional
+                else { return nil }
+            // otherwise if we get anything back return the first one
+            return results.first
+        } catch {
+            print("Error for request.")
+        }
+        
+        return nil
+    }
 }
