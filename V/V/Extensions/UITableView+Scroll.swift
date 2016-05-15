79d6e293f2d1671149af24c8c2779b0303dc3fde
Dulio Denis <dulio.denis@yahoo.com>
2016-05-15 11:08:39 -0400

    updated scrolling to account for numberOfSections

--- V/V/Extensions/UITableView+Scroll.swift
+++ V/V/Extensions/UITableView+Scroll.swift
@@ -12,8 +12,18 @@ extension UITableView {
     
     // Scroll a tableView down to the bottom
     func scrollToBottom() {
-        // Only scroll down if we have rows in the tableView
-        if numberOfRowsInSection(0) > 0 {
+        // if the number of sections is greater than 1
+        if self.numberOfSections > 1 {
+            // then determine what the last section is
+            let lastSection = self.numberOfSections - 1
+            // generate an IndexPath for that last section
+            let indexPath = NSIndexPath(forRow: self.numberOfRowsInSection(lastSection) - 1, inSection: lastSection)
+            // and scroll to the bottom of that index path
+            self.scrollToRowAtIndexPath(indexPath, atScrollPosition: .Bottom, animated: true)
+        }
+        
+        // else scroll down if we have rows in the tableView and only one section
+        else if numberOfRowsInSection(0) > 0 && self.numberOfSections == 1 {
             self.scrollToRowAtIndexPath(NSIndexPath(forRow: self.numberOfRowsInSection(0) - 1, inSection: 0), atScrollPosition: .Bottom, animated: true)
         }
     }
