d66e507128accd1759db66d2a21f24efd3069f02
Dulio Denis <dulio.denis@yahoo.com>
2016-05-17 11:20:03 -0400

    Add constraints to the ChatCell

--- V/V/Views/ChatCell.swift
+++ V/V/Views/ChatCell.swift
@@ -25,13 +25,32 @@ class ChatCell: UITableViewCell {
         
         // setup an array with our three label instances
         let labels = [nameLabel, messageLabel, dateLabel]
-        // iterate through the label arrary
+        // iterate through the label array
         for label in labels {
             // turn off translates autoresizing mask into contstraint
             label.translatesAutoresizingMaskIntoConstraints = false
             // and add to the content view
             contentView.addSubview(label)
         }
+        
+        // setup constraints in an array for activation
+        let constraints: [NSLayoutConstraint] = [
+            // setup the name label to be constraint to the top anchor and leading anchor of the content view
+            nameLabel.topAnchor.constraintEqualToAnchor(contentView.layoutMarginsGuide.topAnchor),
+            nameLabel.leadingAnchor.constraintEqualToAnchor(contentView.layoutMarginsGuide.leadingAnchor),
+            
+            // constrain the message label to the bottom of the content view and the same leading anchor as the name label
+            messageLabel.bottomAnchor.constraintEqualToAnchor(contentView.layoutMarginsGuide.bottomAnchor),
+            messageLabel.leadingAnchor.constraintEqualToAnchor(nameLabel.leadingAnchor),
+            
+            // constrain the date label to the trailing anchor of the content view 
+            // and align to the first base line anchor of the name label
+            dateLabel.trailingAnchor.constraintEqualToAnchor(contentView.layoutMarginsGuide.trailingAnchor),
+            dateLabel.firstBaselineAnchor.constraintEqualToAnchor(nameLabel.firstBaselineAnchor)
+        ]
+        
+        // activate the constraints with the constraint array
+        NSLayoutConstraint.activateConstraints(constraints)
     }
 
     required init?(coder aDecoder: NSCoder) {
