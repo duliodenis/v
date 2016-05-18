60f1446b56731c8b6ea0ea5b6ba56b2cf9177118
Dulio Denis <dulio.denis@yahoo.com>
2016-05-18 17:09:35 -0400

    Add a tableView with constraints to the AllChatsVC

--- V/V/Controllers/AllChatsViewController.swift
+++ V/V/Controllers/AllChatsViewController.swift
@@ -16,4 +16,38 @@ class AllChatsViewController: UIViewController {
     
     private let tableView = UITableView(frame: CGRectZero, style: .Plain)
     private let cellIdentifier = "MessageCell"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set navbar title and new chat bar button item
+        title = "Chats"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "new_chat"), style: .Plain, target: self, action: "newChat")
+        
+        // ensure the tableview isn't distorted due to the navbar
+        automaticallyAdjustsScrollViewInsets = false
+        
+        // tableView setup: register class, initialize tableview footer, allow our use of auto-layout
+        tableView.registerClass(ChatCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.tableFooterView = UIView(frame: CGRectZero)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        // add constraints to the tableview
+        let tableViewConstraints: [NSLayoutConstraint] = [
+            tableView.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor),
+            tableView.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor),
+            tableView.bottomAnchor.constraintEqualToAnchor(bottomLayoutGuide.topAnchor)
+        ]
+        
+        // activate the constraints
+        NSLayoutConstraint.activateConstraints(tableViewConstraints)
+    }
+    
+
+    func newChat() {
+        
+    }
 }
