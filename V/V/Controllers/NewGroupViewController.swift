761f3aa8e26d813a329efb987e5f6d1cc9f8bf93
Dulio Denis <dulio.denis@yahoo.com>
2016-06-18 16:32:10 -0400

    Start NewGroupVC set-up

--- V/V/Controllers/NewGroupViewController.swift
+++ V/V/Controllers/NewGroupViewController.swift
@@ -15,10 +15,46 @@ class NewGroupViewController: UIViewController {
     // set this when we create our new instance of our VC
     var chatCreationDelegate: ChatCreationDelegate?
     
+    private let subjectField = UITextField()
+    private let characterNumberLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // set the title
         title = "New Group"
+        
+        // set the background color
+        view.backgroundColor = UIColor.whiteColor()
+        // set the left and right Navigation Bar Button Items to Cancel & Next
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .Plain, target: self, action: "cancel")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .Plain, target: self, action: "next")
+        
+        // set the placeholder and delegate for our Subject Field variable
+        subjectField.placeholder = "Group Subject"
+        subjectField.delegate = self
+        // falsify the automatic constraints so we can use Auto Layout
+        subjectField.translatesAutoresizingMaskIntoConstraints = false
+        // add the subjectField to the view
+        view.addSubview(subjectField)
+        
+        // setup the characterNumberLabel view 
+        characterNumberLabel.textColor = UIColor.grayColor()
+        characterNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        // add the characterNumberLabel as a sub view to our subject field
+        subjectField.addSubview(characterNumberLabel)
+        
+        // add an Auto Layout constrainable light gray bottom border
+        let bottomBorder = UIView()
+        bottomBorder.backgroundColor = UIColor.lightGrayColor()
+        bottomBorder.translatesAutoresizingMaskIntoConstraints = false
+        // make bottom border a sub view of the subject field
+        subjectField.addSubview(bottomBorder)
     }
+    
+}
+
+
+extension NewGroupViewController: UITextFieldDelegate {
     
 }
