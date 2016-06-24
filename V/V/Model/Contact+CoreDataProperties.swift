582ae9004e684605ac2e3cc371be803f9cfb570d
Dulio Denis <dulio.denis@yahoo.com>
2016-06-24 17:29:36 -0400

    Update Contact Model to include a contactID

--- V/V/Model/Contact+CoreDataProperties.swift
+++ V/V/Model/Contact+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Contact+CoreDataProperties.swift
 //  V
 //
-//  Created by Dulio Denis on 6/12/16.
+//  Created by Dulio Denis on 6/24/16.
 //  Copyright © 2016 Dulio Denis. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -16,6 +16,7 @@ extension Contact {
 
     @NSManaged var firstName: String?
     @NSManaged var lastName: String?
+    @NSManaged var contactID: String?
     @NSManaged var chats: NSSet?
     @NSManaged var messages: NSSet?
 
