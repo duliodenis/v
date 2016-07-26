35353e82fa1d989c63498fbdbcb595de692c95cc
Dulio Denis <dulio.denis@yahoo.com>
2016-07-25 23:15:03 -0400

    Added FavoriteCell Class and Icons

---
+++ V/V/Views/FavoriteCell.swift
@@ -0,0 +1,14 @@
+//
+//  FavoriteCell.swift
+//  V
+//
+//  Created by Dulio Denis on 7/25/16.
+//  Copyright © 2016 Dulio Denis. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+
+}
