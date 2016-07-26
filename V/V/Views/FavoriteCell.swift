fde0a305a00d08a29f996e7bd0dd2e76971fcb6d
Dulio Denis <dulio.denis@yahoo.com>
2016-07-26 09:44:54 -0400

    Implement FavoriteCell Class

--- V/V/Views/FavoriteCell.swift
+++ V/V/Views/FavoriteCell.swift
@@ -10,5 +10,26 @@ import UIKit
 
 class FavoriteCell: UITableViewCell {
 
+    let phoneTypeLabel = UILabel()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .Subtitle, reuseIdentifier: reuseIdentifier)
+        
+        // set some colors to the text labels
+        detailTextLabel?.textColor = UIColor.lightGrayColor()
+        phoneTypeLabel.textColor = UIColor.lightGrayColor()
+        // update the translatesAutoresizingMaskIntoConstraints to use auto-layout
+        phoneTypeLabel.translatesAutoresizingMaskIntoConstraints = false
+        // add our phonetype label to the subview
+        contentView.addSubview(phoneTypeLabel)
+        
+        // add centerY and trailing anchor constraints to phonetype label and activate
+        phoneTypeLabel.centerYAnchor.constraintEqualToAnchor(contentView.centerYAnchor).active = true
+        phoneTypeLabel.trailingAnchor.constraintEqualToAnchor(contentView.layoutMarginsGuide.trailingAnchor).active = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented.")
+    }
 
 }
