da60ae87998314e6d86b2777eca6c8462a1adc30
Dulio Denis <dulio.denis@yahoo.com>
2016-08-14 09:57:21 -0400

    Create configureCellMethod in FavoritesViewController

--- V/V/Controllers/FavoritesViewController.swift
+++ V/V/Controllers/FavoritesViewController.swift
@@ -22,4 +22,22 @@ class FavoritesViewController: UIViewController, TableViewFetchedResultsDisplaye
     
     private let store = CNContactStore()
     
+    
+    func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
+        // get the relevant contact back from the fetched results controller 
+        // using the object at index path method
+        guard let contact = fetchedResultsController?.objectAtIndexPath(indexPath) as? Contact else {return}
+        
+        // use a guard statement to confirm the cell is a favorite cell
+        guard let cell = cell as? FavoriteCell else {return}
+        
+        // if it is update the textlabel, detail text label, phone type label
+        cell.textLabel?.text = contact.fullName
+        cell.detailTextLabel?.text = "*** No Status ***"
+        cell.phoneTypeLabel.text = "mobile"
+        
+        // add an accessory type using the Detail Button enum type
+        cell.accessoryType = .DetailButton
+    }
+    
 }
