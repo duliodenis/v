72a817a2cbf9f1c8f5d62fc6aa8c00f96946dcc1
Dulio Denis <dulio.denis@yahoo.com>
2016-08-14 12:45:36 -0400

    Update Contact and PhoneNumber Models

--- V/V/Model/PhoneNumber+CoreDataProperties.swift
+++ V/V/Model/PhoneNumber+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  PhoneNumber+CoreDataProperties.swift
 //  V
 //
-//  Created by Dulio Denis on 6/26/16.
+//  Created by Dulio Denis on 8/14/16.
 //  Copyright © 2016 Dulio Denis. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -15,6 +15,8 @@ import CoreData
 extension PhoneNumber {
 
     @NSManaged var value: String?
+    @NSManaged var kind: String?
+    @NSManaged var registered: Bool
     @NSManaged var contact: Contact?
 
 }
