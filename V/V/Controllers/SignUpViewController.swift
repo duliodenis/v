9ec6247f87fa739559dbdb79c879a937aa94949f
Dulio Denis <dulio.denis@yahoo.com>
2016-08-16 13:13:20 -0400

    Added UIColor Extension for Flat Colors

---
+++ V/V/Controllers/SignUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpViewController.swift
+//  V
+//
+//  Created by Dulio Denis on 8/16/16.
+//  Copyright © 2016 Dulio Denis. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ V/V/Extensions/UIColor+FlatColors.swift
@@ -0,0 +1,201 @@
+//
+//  UIColor+FlatColors.swift
+//  V
+//
+//  Created by Dulio Denis on 8/16/16.
+//  Copyright © 2016 Dulio Denis. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    // light shades
+    
+    class func flatBlackColor() -> UIColor {
+        return UIColor(red: 0/255, green: 0/255, blue: 17/255, alpha: 1)
+    }
+    
+    class func flatBlueColor() -> UIColor {
+        return UIColor(red: 224/255, green: 50/255, blue: 63/255, alpha: 1)
+    }
+    
+    class func flatBrownColor() -> UIColor {
+        return UIColor(red: 24/255, green: 45/255, blue: 37/255, alpha: 1)
+    }
+    
+    class func flatCoffeeColor() -> UIColor {
+        return UIColor(red: 25/255, green: 31/255, blue: 64/255, alpha: 1)
+    }
+    
+    class func flatForestGreenColor() -> UIColor {
+        return UIColor(red: 138/255, green: 45/255, blue: 37/255, alpha: 1)
+    }
+    
+    class func flatGrayColor() -> UIColor {
+        return UIColor(red: 184/255, green: 10/255, blue: 65/255, alpha: 1)
+    }
+    
+    class func flatGreenColor() -> UIColor {
+        return UIColor(red: 145/255, green: 77/255, blue: 80/255, alpha: 1)
+    }
+    
+    class func flatMagentaColor() -> UIColor {
+        return UIColor(red: 283/255, green: 51/255, blue: 71/255, alpha: 1)
+    }
+    
+    class func flatMaroonColor() -> UIColor {
+        return UIColor(red: 5/255, green: 65/255, blue: 47/255, alpha: 1)
+    }
+    
+    class func flatMintColor() -> UIColor {
+        return UIColor(red: 168/255, green: 86/255, blue: 74/255, alpha: 1)
+    }
+    
+    class func flatNavyBlueColor() -> UIColor {
+        return UIColor(red: 210/255, green: 45/255, blue: 37/255, alpha: 1)
+    }
+    
+    class func flatOrangeColor() -> UIColor {
+        return UIColor(red: 28/255, green: 85/255, blue: 90/255, alpha: 1)
+    }
+    
+    class func flatPinkColor() -> UIColor {
+        return UIColor(red: 324/255, green: 49/255, blue: 96/255, alpha: 1)
+    }
+    
+    class func flatPlumColor() -> UIColor {
+        return UIColor(red: 300/255, green: 45/255, blue: 37/255, alpha: 1)
+    }
+    
+    class func flatPowderBlueColor() -> UIColor {
+        return UIColor(red: 222/255, green: 24/255, blue: 95/255, alpha: 1)
+    }
+    
+    class func flatPurpleColor() -> UIColor {
+        return UIColor(red: 253/255, green: 52/255, blue: 77/255, alpha: 1)
+    }
+    
+    class func flatRedColor() -> UIColor {
+        return UIColor(red: 6/255, green: 74/255, blue: 91/255, alpha: 1)
+    }
+    
+    class func flatSandColor() -> UIColor {
+        return UIColor(red: 42/255, green: 25/255, blue: 94/255, alpha: 1)
+    }
+    
+    class func flatSkyBlueColor() -> UIColor {
+        return UIColor(red: 204/255, green: 76/255, blue: 86/255, alpha: 1)
+    }
+    
+    class func flatTealColor() -> UIColor {
+        return UIColor(red: 195/255, green: 55/255, blue: 51/255, alpha: 1)
+    }
+    
+    class func flatWatermelonColor() -> UIColor {
+        return UIColor(red: 356/255, green: 53/255, blue: 94/255, alpha: 1)
+    }
+    
+    class func flatWhiteColor() -> UIColor {
+        return UIColor(red: 192/255, green: 2/255, blue: 95/255, alpha: 1)
+    }
+    
+    class func flatYellowColor() -> UIColor {
+        return UIColor(red: 48/255, green:99/255, blue: 100/255, alpha: 1)
+    }
+    
+    // dark shades
+    
+    class func flatBlackColorDark() -> UIColor {
+        return UIColor(red: 0/255, green: 0/255, blue: 15/255, alpha: 1)
+    }
+    
+    class func flatBlueColorDark() -> UIColor {
+        return UIColor(red: 224/255, green: 56/255, blue: 51/255, alpha: 1)
+    }
+    
+    class func flatBrownColorDark() -> UIColor {
+        return UIColor(red: 25/255, green: 45/255, blue: 31/255, alpha: 1)
+    }
+    
+    class func flatCoffeeColorDark() -> UIColor {
+        return UIColor(red: 25/255, green: 34/255, blue: 56/255, alpha: 1)
+    }
+    
+    class func flatForestGreenColorDark() -> UIColor {
+        return UIColor(red: 135/255, green: 44/255, blue: 31/255, alpha: 1)
+    }
+    
+    class func flatGrayColorDark() -> UIColor {
+        return UIColor(red: 184/255, green: 10/255, blue: 55/255, alpha: 1)
+    }
+    
+    class func flatGreenColorDark() -> UIColor {
+        return UIColor(red: 145/255, green: 78/255, blue: 68/255, alpha: 1)
+    }
+    
+    class func flatMagentaColorDark() -> UIColor {
+        return UIColor(red: 282/255, green: 61/255, blue: 68/255, alpha: 1)
+    }
+    
+    class func flatMaroonColorDark() -> UIColor {
+        return UIColor(red: 4/255, green: 68/255, blue: 40/255, alpha: 1)
+    }
+    
+    class func flatMintColorDark() -> UIColor {
+        return UIColor(red: 168/255, green: 86/255, blue: 63/255, alpha: 1)
+    }
+    
+    class func flatNavyBlueColorDark() -> UIColor {
+        return UIColor(red: 210/255, green: 45/255, blue: 31/255, alpha: 1)
+    }
+    
+    class func flatOrangeColorDark() -> UIColor {
+        return UIColor(red: 24/255, green: 100/255, blue: 83/255, alpha: 1)
+    }
+    
+    class func flatPinkColorDark() -> UIColor {
+        return UIColor(red: 327/255, green: 57/255, blue: 83/255, alpha: 1)
+    }
+    
+    class func flatPlumColorDark() -> UIColor {
+        return UIColor(red: 300/255, green: 46/255, blue: 31/255, alpha: 1)
+    }
+    
+    class func flatPowderBlueColorDark() -> UIColor {
+        return UIColor(red: 222/255, green: 28/255, blue: 84/255, alpha: 1)
+    }
+    
+    class func flatPurpleColorDark() -> UIColor {
+        return UIColor(red: 253/255, green: 56/255, blue: 64/255, alpha: 1)
+    }
+    
+    class func flatRedColorDark() -> UIColor {
+        return UIColor(red: 6/255, green: 78/255, blue: 75/255, alpha: 1)
+    }
+    
+    class func flatSandColorDark() -> UIColor {
+        return UIColor(red: 42/255, green: 30/255, blue: 84/255, alpha: 1)
+    }
+    
+    class func flatSkyBlueColorDark() -> UIColor {
+        return UIColor(red: 204/255, green: 78/255, blue: 73/255, alpha: 1)
+    }
+    
+    class func flatTealColorDark() -> UIColor {
+        return UIColor(red: 196/255, green: 54/255, blue: 45/255, alpha: 1)
+    }
+    
+    class func flatWatermelonColorDark() -> UIColor {
+        return UIColor(red: 358/255, green: 61/255, blue: 85/255, alpha: 1)
+    }
+    
+    class func flatWhiteColorDark() -> UIColor {
+        return UIColor(red: 204/255, green: 5/255, blue: 78/255, alpha: 1)
+    }
+    
+    class func flatYellowColorDark() -> UIColor {
+        return UIColor(red: 40/255, green: 100/255, blue: 100/255, alpha: 1)
+    }
+    
+}
