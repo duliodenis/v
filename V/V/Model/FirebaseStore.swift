7ad5a81ca96b0662debaa6741c66c1b5febbd8ab
Dulio Denis <dulio.denis@yahoo.com>
2016-08-19 07:20:04 -0400

    FirebaseStore: Add extension to Conform to the RemoteStore protocol

--- V/V/Model/FirebaseStore.swift
+++ V/V/Model/FirebaseStore.swift
@@ -23,3 +23,21 @@ class FirebaseStore {
         return rootRef.authData != nil
     }
 }
+
+extension FirebaseStore: RemoteStore {
+    
+    func startSyncing() {
+        
+    }
+    
+    
+    func store(inserted inserted: [NSManagedObject], updated: [NSManagedObject], deleted: [NSManagedObject]) {
+        
+    }
+    
+    
+    func signUp(phoneNumber phoneNumber: String, email: String, password: String, success: () -> (), error: (errorMessage: String) -> ()) {
+        
+    }
+    
+}
