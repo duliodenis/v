d10d557f3c62993ed6eed46b046b4e80c7e1c2b3
Dulio Denis <dulio.denis@yahoo.com>
2016-08-20 13:06:44 -0400

    Add an extension to Contacts to conform to the FirebaseModel Protocol

--- V/V/Protocols/FirebaseModel.swift
+++ V/V/Protocols/FirebaseModel.swift
@@ -15,3 +15,23 @@ protocol FirebaseModel {
     func upload(rootRef: Firebase, context: NSManagedObjectContext)
     
 }
+
+
+extension Contact: FirebaseModel {
+
+    // upload contact instances from Firebase into our Core Data Model
+    
+    func upload(rootRef: Firebase, context: NSManagedObjectContext) {
+        guard let phoneNumbers = phoneNumbers?.allObjects as? [PhoneNumber] else {return}
+        
+        for number in phoneNumbers {
+            rootRef.childByAppendingPath("users").queryOrderedByChild("phoneNumber").queryEqualToValue(number.value).observeSingleEventOfType(.Value, withBlock: {
+                snapshot in
+                
+            })
+            
+            
+        }
+    }
+    
+}
