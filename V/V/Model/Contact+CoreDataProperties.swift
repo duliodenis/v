943d7abc025f2fb1c206e9aaf5cb6299b9e67fcb
Dulio Denis <dulio.denis@yahoo.com>
2016-08-20 09:13:14 -0400

    Updated Contact Model to include a storageID

--- V/V/Model/Contact+CoreDataProperties.swift
+++ V/V/Model/Contact+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Contact+CoreDataProperties.swift
 //  V
 //
-//  Created by Dulio Denis on 8/14/16.
+//  Created by Dulio Denis on 8/20/16.
 //  Copyright © 2016 Dulio Denis. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -15,10 +15,11 @@ import CoreData
 extension Contact {
 
     @NSManaged var contactID: String?
+    @NSManaged var favorite: Bool
     @NSManaged var firstName: String?
     @NSManaged var lastName: String?
-    @NSManaged var favorite: Bool
     @NSManaged var status: String?
+    @NSManaged var storageID: String?
     @NSManaged var chats: NSSet?
     @NSManaged var messages: NSSet?
     @NSManaged var phoneNumbers: NSSet?
